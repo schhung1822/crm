004e512ca1a6b612b099d166d47741d29526bd01
Leo Hunq <schhung2143@gmail.com>
2026-01-28 16:04:18 +0700

    Redirect default entry to events

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/dashboard",
-        destination: "/dashboard/default",
+        destination: "/events",
         permanent: false,
       },
     ];
